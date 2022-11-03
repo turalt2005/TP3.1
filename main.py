b89f56a554c0c9d92d099377d65c6c498ba4e910
turalt2005 <112487805+turalt2005@users.noreply.github.com>
2022-11-03 16:11:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Quit = 'n'
 while Quit == 'n':
     x = random.randint (1, 5)
     y = random.randint (1, 6)
+    z = random.randint (1, 6)
     nb_vies = 20
     print("Bienvenue! Danss ce jeu, tu auras 20 vies pour combattre le plus de monstres possible."
           "Si vos vies descendent a 0, c'est partie terminer")
